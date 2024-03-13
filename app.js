b65dd8e27b08b406fe176842ed2f5e4a6385174c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-03-13 21:40:59 +0530

    push code

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 let students = ["dhruv", "Rathod", "Disha"];
 
 let cars = ["mahindra", "xuv"];
+
+let str = prompt("please enter a string");
+
+if (str.length == 0 ) {
+    console.log("string is empty");
+} else {
+    console.log("string is not empty");
+}
+
+console.log(str);
